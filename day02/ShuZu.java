69ba07df9eb116a152977b951ca7f87619f37328
liangxiuwei <1664500281@qq.com>
2015-07-08 19:34:40 +0800

    作业

---
+++ day02/ShuZu.java
@@ -0,0 +1,34 @@
+
+
+public class ShuZu {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] arr=new int[20][7];
+		String[] biaoTi={"coreC++","coreJava","Servlet","JSP","EJB","总分","平均分"};
+		for(int i=0;i<biaoTi.length;i++)
+		{
+			System.out.print(biaoTi[i]+"  ");
+		}
+		System.out.println();
+		for(int h=0;h<arr.length;h++)
+		{
+			int m=0;
+			for(int l=0;l<5;l++)
+			{
+				arr[h][l]=(int)(Math.random()*90+10);
+				m=m+arr[h][l];
+				System.out.print("  "+arr[h][l]+"        ");
+			}
+			System.out.print(m);
+			System.out.print("        "+m/(arr[h].length-2));
+			System.out.println();
+		}
+	}
+}
+
+
+
