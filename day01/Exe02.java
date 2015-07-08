e9e2b622b6fb92f9188f2d1b59fce29fd31c7616
liangxiuwei <1664500281@qq.com>
2015-07-08 13:05:24 +0800

    day01
    任务

---
+++ day01/Exe02.java
@@ -0,0 +1,17 @@
+package haowan;
+
+public class Exe02 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch1='A';
+		char ch2='中';
+		System.out.println("ch1的值为"+ch1);
+		System.out.println("ch2的值为"+ch2);
+		
+	}
+
+}
---
+++ day01/JJCC.java
@@ -0,0 +1,56 @@
+
+public class JJCC {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a=2;
+    int b=3;
+    byte b1=30;
+    byte b2=40;
+    char ch1='a';
+    char ch2=77;
+    long l1=490000000;
+    long l2=5099;
+    float f1=3.0f;
+    float f2=4.0f;
+    double d1=2.0;
+    double d2=4.5;
+    boolean e=false;
+    boolean g=true;
+    short s1=9;
+    short s2=7;
+    System.out.println("a+b="+(a+b));
+    System.out.println("a-b="+(a-b));
+    System.out.println("a*b="+(a*b));
+    System.out.println("a/b="+(a/b));
+    System.out.println("b1+b2="+(b1+b2));
+    System.out.println("b1-b2="+(b1-b2));
+    System.out.println("b1*b2="+(b1*b2));
+    System.out.println("b1/b2="+(b1/b2));
+    System.out.println("ch1+ch2="+(ch1+ch2));
+    System.out.println("ch1-ch2="+(ch1-ch2));
+    System.out.println("ch1*ch2="+(ch1*ch2));
+    System.out.println("ch1/ch2="+(ch1/ch2));
+    System.out.println("l1+l2="+(l1+l2));
+    System.out.println("l1-l2="+(l1-l2));
+    System.out.println("l1*l2="+(l1*l2));
+    System.out.println("l1/l2="+(l1/l2));
+    System.out.println("f1+f2="+(f1+f2));
+    System.out.println("f1-f2="+(f1-f2));
+    System.out.println("f1*f2="+(f1*f2));
+    System.out.println("f1/f2="+(f1/f2));
+    System.out.println("d1+d2="+(d1+d2));
+    System.out.println("d1-d2="+(d1-d2));
+    System.out.println("d1*d2="+(d1*d2));
+    System.out.println("d1/d2="+(d1/d2));
+    System.out.println("s1+s2="+(s1+s2));
+    System.out.println("s1-s2="+(s1-s2));
+    System.out.println("s1*s2="+(s1*s2));
+    System.out.println("s1/s2="+(s1/s2));
+    
+	}
+
+}
